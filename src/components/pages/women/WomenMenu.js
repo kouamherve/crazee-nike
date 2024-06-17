13d2cbb624fc136d21f7caa341b9d82dc08cc0d5
Herve Kouam <kouamherve01@gmail.com>
2024-06-17 17:45:07 -0400

    ✨ F01: ALLMENUS > Add  WomenMenu with style

--- src/components/pages/women/WomenMenu.js
+++ src/components/pages/women/WomenMenu.js
@@ -1,5 +1,34 @@
-import React from "react";
+import { RxChevronRight } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
 export default function WomenMenu() {
-  return <div>WomenPage</div>;
+  const womens = [
+    { title: "Featured" },
+    { title: "Collections" },
+    { title: "Shoes" },
+    { title: "Clothing" },
+    { title: "Accessories" },
+    { title: "Shop by Sport" },
+  ];
+  return (
+    <div className=" mt-4 py-6 font-medium">
+      <Link className=" py-2 text-[22px] ">Women</Link>
+      <div className=" pt-8 pb-40 text-base text-secondary">
+        <div className="flex flex-col gap-5">
+          <Link>New Arrivals</Link>
+          <Link>Up to 50 % Off Select Style </Link>
+        </div>
+        <ul className=" py-5 space-y-5">
+          {womens.map((n) => (
+            <li key={n.title}>
+              <div className="flex items-center justify-between">
+                <h3 className=" font-medium capitalize">{n.title}</h3>
+                <RxChevronRight className=" text-black text-[22px]" />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
