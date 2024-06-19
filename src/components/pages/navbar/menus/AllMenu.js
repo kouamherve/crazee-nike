6eaa24f6cfad907a850bddebf6ecc6306c98a7e1
Herve Kouam <kouamherve01@gmail.com>
2024-06-18 20:40:38 -0400

    ♻️  F01: ALLMENUS > Add AllMenuProduct

--- src/components/pages/navbar/menus/AllMenu.js
+++ src/components/pages/navbar/menus/AllMenu.js
@@ -1,11 +1,10 @@
-import { RxChevronRight } from "react-icons/rx";
 import ExtraProduct from "../../../reusable-ui/ExtraProduct";
 import AllMenuForm from "./AllMenuForm";
 import AllMenuFooter from "./AllMenuFooter";
-import { allMenus } from "./helpers";
 import NavbarMenu from "../../../reusable-ui/NavbarMenu";
 import { useContext, useState } from "react";
 import MenuContext from "../../../../context/MenuContext";
+import AllMenuProduct from "./AllMenuProduct";
 
 export default function AllMenu() {
   const [content, setContent] = useState("");
@@ -15,24 +14,7 @@ export default function AllMenu() {
   return (
     <NavbarMenu isHome="homepage">
       <div>
-        {/* <div className="mt-4 py-6">
-          <ul className=" text-[22px] space-y-3">
-            {allMenus.map((menu) => (
-              <li key={menu.id}>
-                <div
-                  onClick={() => {
-                    setIsHidden(true);
-                    setContent(menu.content);
-                  }}
-                  className="flex items-center justify-between"
-                >
-                  <h3 className=" font-medium capitalize">{menu.title}</h3>
-                  <RxChevronRight />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div> */}
+        <AllMenuProduct setContent={setContent} setIsHidden={setIsHidden} />
         <div className="py-6 space-y-4">
           <ExtraProduct title="Jordan" />
           <ExtraProduct title="Converse" />
---
+++ src/components/pages/navbar/menus/AllMenuProduct.js
@@ -0,0 +1,25 @@
+import { allMenus } from "./helpers";
+import { RxChevronRight } from "react-icons/rx";
+
+export default function AllMenuProduct({ setContent, setIsHidden }) {
+  return (
+    <div className="mt-4 py-6">
+      <ul className=" text-[22px] space-y-3">
+        {allMenus.map((menu) => (
+          <li key={menu.id}>
+            <div
+              onClick={() => {
+                setIsHidden(true);
+                setContent(menu.content);
+              }}
+              className="flex items-center justify-between"
+            >
+              <h3 className=" font-medium capitalize">{menu.title}</h3>
+              <RxChevronRight />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
