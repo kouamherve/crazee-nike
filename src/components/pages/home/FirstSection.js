e5dfe8d5d3a8d5b44f4925a4d8b13ab2eba1285e
Herve Kouam <kouamherve01@gmail.com>
2024-07-03 22:03:41 -0400

    ✨ F02: HOMEPAGE > FirstSection updated

--- src/components/pages/home/FirstSection.js
+++ src/components/pages/home/FirstSection.js
@@ -1,27 +1,14 @@
-import Button from "../../reusable-ui/Button";
-
 export default function FirstSection() {
   return (
-    <section>
-      <div className=" h-[505px] w-full">
-        <img
-          className=" h-full w-full object-cover object-center"
-          src="https://cdn.pixabay.com/photo/2020/07/19/05/31/nike-5418992_1280.jpg"
-          alt=""
-        />
-      </div>
-      <div className=" px-9 pt-6 pb-12 text-center">
-        <h1 className=" text-4xl py-2 tracking-tighter font-Lilita font-medium uppercase">
-          producer pack
-        </h1>
-        <p className=" p-2 font-medium">
-          The Alchemist, Bink! and Metro Boomin highlight the natural connection
-          between Hoop and Hip Hop with their own custom versions of the KD17.
-        </p>
-        <Button className=" mt-3 bg-black border-black/60 text-white">
-          Shop
-        </Button>
-      </div>
-    </section>
+    <div className="w-full h-[500px] bg-dark">
+      <video
+        className="w-full h-full object-cover"
+        src="https://videos.pexels.com/video-files/5553467/5553467-sd_640_360_24fps.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+    </div>
   );
 }
--- src/components/pages/navbar/Navbar.js
+++ src/components/pages/navbar/Navbar.js
@@ -3,10 +3,8 @@ import Menus from "./menus/Menus";
 
 export default function Navbar() {
   return (
-    <div className=" relative">
-      <nav
-        className={` w-full fixed z-30 font-Montserrat bg-dark overflow-x-hidden`}
-      >
+    <div className=" relative bg-dark">
+      <nav className={` w-full  z-30 font-Montserrat overflow-x-hidden`}>
         <div className="flex items-center justify-between px-6 py-5 shadow">
           <Logo />
           <Menus />
