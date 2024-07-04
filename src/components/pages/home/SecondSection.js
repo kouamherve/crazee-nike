ff8fd158351650b0fd9c1e7c3d7ecb8ef3faf5f0
Herve Kouam <kouamherve01@gmail.com>
2024-07-03 22:56:57 -0400

    ✨ F02: HOMEPAGE > SecondSection updated

--- src/components/pages/home/SecondSection.js
+++ src/components/pages/home/SecondSection.js
@@ -1,32 +1,48 @@
-import HomeCard from "../../reusable-ui/HomeCard";
+import Button from "../../reusable-ui/Button";
 
 export default function SecondSection() {
   return (
-    <section>
-      <HomeCard
-        className=" h-[500px]"
-        detail="Just Dropped"
-        title="The Aura Bag"
-        image="https://cdn.pixabay.com/photo/2015/04/24/20/57/girl-738302_1280.jpg"
-      />
-      <HomeCard
-        className=" h-[500px]"
-        detail="New Arrivals"
-        title="Air Jordan 4 'Oxidized Green'"
-        image="https://cdn.pixabay.com/photo/2020/09/17/06/28/sneakers-5578127_1280.jpg"
-      />
-      <HomeCard
-        className=" h-[500px]"
-        detail="Don't Waste Your Energy"
-        title="Run in Pegasus 41"
-        image="https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_1280.jpg"
-      />
-      <HomeCard
-        className=" h-[500px]"
-        detail="Look Good, Play Good"
-        title="Prime Time Gear"
-        image="https://cdn.pixabay.com/photo/2020/05/27/07/40/nike-5226090_1280.jpg"
-      />
-    </section>
+    <div className="  bg-dark">
+      <div className="px-9 py-16 text-center text-white">
+        <h1 className=" text-[40px] pb-2 tracking-tighter font-Lilita font-medium uppercase">
+          Win on air
+        </h1>
+        <p className=" p-2 font-medium">
+          Meet the next generation of Crazee-Nike Air. Engineered to the exact
+          specifications of championship athletes.
+        </p>
+        <div className=" space-x-2 pt-4 text-dark ">
+          <Button className=" bg-white border border-white">
+            Experience Air
+          </Button>
+          <Button className="  bg-white border border-white">Shop Air</Button>
+        </div>
+      </div>
+      <div>
+        <div className="w-full h-[450px]">
+          <img
+            className=" w-full h-full object-cover object-center"
+            src="https://cdn.pixabay.com/photo/2020/07/19/05/31/nike-5418992_1280.jpg"
+            alt=""
+          />
+        </div>
+        <div className="flex w-full h-[300px]">
+          <div className=" w-full h-full">
+            <img
+              className=" w-full h-full object-cover object-center"
+              src="https://cdn.pixabay.com/photo/2015/05/06/21/55/soccer-755825_1280.jpg"
+              alt=""
+            />
+          </div>
+          <div className=" w-full h-full">
+            <img
+              className=" w-full h-full object-cover object-center"
+              src="https://cdn.pixabay.com/photo/2017/06/07/11/33/football-2380100_1280.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
