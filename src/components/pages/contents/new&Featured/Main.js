b8b50e3b6c3c57bb590bd5d07a1ac52ec5d928fb
Herve Kouam <kouamherve01@gmail.com>
2024-07-19 15:06:22 -0400

    ♻️ F02: APP > NEW&FEATURED > Add Main

---
+++ src/components/pages/contents/new&Featured/Main.js
@@ -0,0 +1,77 @@
+import NavbarSticky from "./NavbarSticky";
+import ReleasesFilter from "./ReleasesFilter";
+import PickUpFeatured from "./PickUpFeatured";
+import Products from "./Products";
+import Button from "../../../reusable-ui/Button";
+
+export default function Main({ showNavbar, isAtTop }) {
+  return (
+    <>
+      <div className="pb-28">
+        <NavbarSticky showNavbar={showNavbar} isAtTop={isAtTop} />
+
+        <ReleasesFilter />
+
+        <PickUpFeatured />
+
+        <Products />
+      </div>
+      <hr className=" border-backgroundHr border-[0.5px]" />
+      <div className="pt-20">
+        <h3 className=" text-xl font-medium px-6">Related Categories</h3>
+        <div className=" pt-6 pb-10 w-full text-xs font-semibold flex gap-2 text-nowrap overflow-x-auto no-scrollbar bg-white">
+          <div className="flex h-full items-center justify-center">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              Best Selling Products
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              Best Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              New Basketball Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              New Soccer Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              New Men's Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              New Running Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              Best Men's Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              New Jordan Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              Best Women's Shoes
+            </Button>
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            <Button className=" border-[0.5px] border-black font-semibold px-4">
+              Best Training & Gym
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
